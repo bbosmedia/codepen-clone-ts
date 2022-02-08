3b823de2ed8d4ade238c63639e081e801dfb5d6e
bbosmedia <bbosmedia@users.noreply.github.com>
2022-02-08 09:52:09 +0500

    Codepen Clone Start

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+	return (
+		<>
+			<div className="pane pane-top"></div>
+			<div className="pane pane-bottom">
+				<iframe title="output" src="" sandbox='allow-scripts' frameBorder="0" width="100%" height="100%"></iframe>
+			</div>
+		</>
+	)
+}
+
+export default App
